eb1f8e9f233004e1c90da53296e39f407ca9c3e7
douglas.santos <douglas.santos@nava.com.br>
2021-09-20 22:02:44 -0300

    criação de end point para observação

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,23 +1,33 @@
-const express = require ( 'express')
-const bodyParser = require('body-parser')
+const express = require ('express')
+const bodyParser = require ('body-parser')
 const app = express()
-//middleware, permite acessar o corpo (req.body) e trata-lo como um objeto JSON
+//middleware, permite acessar o corpo (req.body) e tratá-lo como um objeto JSON
 app.use(bodyParser.json())
 
 const lembretes = {}
-contator = 0
+const observacao = {}
+contador = 0
+contadorObs = 0
 
 app.get('/lembretes', (req, res) => {
     res.status(200).send(lembretes)
-
 })
 
-app.post('/lembretes', (req, res)=>{
+app.post ('/lembretes', (req, res) => {
     contador++
     const {texto} = req.body
-    lembretes[contador] = {contator: contador, texto: texto}
-    res.status(201).send(lembretes[contator])
+    lembretes[contador] = {contador: contador, texto: texto, observacao: {}}
+    res.status(201).send(lembretes[contador])
 })
 
+app.get('/seachLembretes', (req, res) => {
+    res.status(200).send(lembretes[req.query.id])
+})
 
-app.listen (4000, () => console.log("Lembretes. Porta 4000"))
+app.post('/inserirObservacao', (req, res) => {
+    contadorObs++
+    lembretes[req.query.id].observacao =  {texto: req.query.id}
+    res.status(201).send(lembretes[contadorObs])
+})
+
+app.listen (4000, () => console.log ("Lembretes. Porta 4000"))
